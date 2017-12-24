952f16e216907ffc8be80370c16ac98837551ad7
acgrid <acgrid@gmail.com>
2017-12-24 15:31:10 +0800

    Migrate to mongodb based storage

--- routes/program.js
+++ routes/program.js
@@ -1,32 +1,30 @@
 const express = require('express'),
     debug = require('debug')('program-store:program'),
     router = express.Router(),
-    path = require('path'),
-    fs = require('fs');
+    Program = require('../lib/program');
 
-const STORAGE = path.join(__dirname, '../', 'storage');
-
-debug(`Storage path: ${STORAGE}`);
-
-router.get('/:channel', function(req, res) {
-    const channel = req.params.channel || 'default';
-    fs.readFile(path.join(STORAGE, `${channel}.json`), (err, data) => {
-        if(err){
-            debug(`Error getting program from ${channel}`, err);
-            res.end(null);
-        }else{
-            debug(`Get program from ${channel}`, data);
-            res.end(data);
-        }
+router.get('/:channel/:id?', function(req, res) {
+    const channel = req.params.channel || 'default', _id = req.params.id;
+    const service = new Program(channel);
+    (_id ? service.fetch(_id) : service.all()).then(data => {
+        res.json(data);
+        debug(`Get program from ${channel}`, data);
+    }).catch(err => {
+        debug(`Error getting program from ${channel}`, err);
+        res.json(null);
     });
 });
 
 router.post('/:channel', function(req, res) {
-    const channel = req.params.channel || 'default', json = req.body;
-    debug(`Save programs to ${channel}`, json);
-    fs.writeFile(path.join(STORAGE, `${channel}.json`), JSON.stringify(json), () => {
-        res.end('ACK');
-    });
+    const channel = req.params.channel || 'default', service = new Program(channel);
+    service.sync(req.body);
+    res.end('ACK');
+});
+
+router.patch('/:channel/:id', function(req, res) {
+    const channel = req.params.channel || 'default', id = req.params.id, service = new Program(channel);
+    if(id) service.update(id, req.body);
+    res.end('ACK');
 });
 
 module.exports = router;
